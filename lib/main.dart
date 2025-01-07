80e42164cb89cc309b17e206ea66e55eca8f7169
AthiraaAthi <athiraa2612@gmail.com>
2025-01-07 18:20:03 +0530

    made debug banner false

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,8 @@ class HospitalApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
