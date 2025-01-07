ab4328c9b8a34742935d65d62cba61768809f6de
AthiraaAthi <athiraa2612@gmail.com>
2025-01-07 18:16:59 +0530

    added new class

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() {}
+
+class HospitalApp extends StatelessWidget {
+  const HospitalApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
