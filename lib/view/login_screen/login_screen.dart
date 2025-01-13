279b0a83aef18521a156b74baf4393b57c941448
AthiraaAthi <athiraa2612@gmail.com>
2025-01-13 18:04:28 +0530

    add spacing below elements in LoginScreen for improved layout

--- lib/view/login_screen/login_screen.dart
+++ lib/view/login_screen/login_screen.dart
@@ -77,6 +77,7 @@ class LoginScreen extends StatelessWidget {
                   ),
                 ),
               ),
+              SizedBox(height: 20),
             ],
           ),
         ),
