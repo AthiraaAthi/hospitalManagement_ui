1462804ddbd6ba94b6b26c9a147512ba19219e19
AthiraaAthi <athiraa2612@gmail.com>
2025-01-17 19:03:43 +0530

    update AppBar layout in HomeScreen with logo and improved title styling

--- lib/view/home_screen/home.dart
+++ lib/view/home_screen/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hospital_ui/utils/color_constant/color_constant.dart';
+import 'package:hospital_ui/utils/img_constant/image_constant.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -8,34 +9,52 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: Icon(Icons.menu),
+        toolbarHeight: 100,
+        leading: Icon(
+          Icons.menu,
+          size: 30,
+        ),
         centerTitle: true,
-        title: Column(
+        title: Row(
           children: [
-            RichText(
-                text: TextSpan(
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            Container(
+              height: 60,
+              width: 60,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(ImageConstant.logo),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Column(
               children: [
-                TextSpan(
-                  text: "HOS",
-                  style: TextStyle(color: black),
-                ),
-                TextSpan(
-                  text: "PI",
-                  style: TextStyle(color: mainGreen),
-                ),
-                TextSpan(
-                  text: "TAL",
-                  style: TextStyle(color: black),
+                RichText(
+                    text: TextSpan(
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                  children: [
+                    TextSpan(
+                      text: "HOS",
+                      style: TextStyle(color: black),
+                    ),
+                    TextSpan(
+                      text: "PI",
+                      style: TextStyle(color: mainGreen),
+                    ),
+                    TextSpan(
+                      text: "TAL",
+                      style: TextStyle(color: black),
+                    ),
+                  ],
+                )),
+                Text(
+                  "MANAGER",
+                  style: TextStyle(
+                    fontSize: 17,
+                    letterSpacing: 14,
+                  ),
                 ),
               ],
-            )),
-            Text(
-              "Manager",
-              style: TextStyle(
-                fontSize: 17,
-                letterSpacing: 10,
-              ),
             ),
           ],
         ),
