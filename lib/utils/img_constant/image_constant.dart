4a79004f49173c7a1c7ec77b01643580f61353a2
AthiraaAthi <athiraa2612@gmail.com>
2025-01-18 19:33:48 +0530

    fix: update image paths in ImageConstant to use forward slashes

--- lib/utils/img_constant/image_constant.dart
+++ lib/utils/img_constant/image_constant.dart
@@ -1,14 +1,14 @@
 class ImageConstant {
   static String logo = "assets/images/hosp_icon.png";
-  static String carousel1 = "assets\images\Slide1.png";
-  static String carousel2 = "assets\images\Slide1.png";
-  static String bookAppoin = "assets\images\BookAppoin.png";
-  static String findDoctor = "assets\images\FindADoc.png";
-  static String labReps = "assets\images\ViewLapReps.png";
-  static String documents = "assets\images\Docs.png";
-  static String appoinList = "assets\images\AppoinList.png";
-  static String uploadDocs = "";
-  static String callForAppoins = "";
-  static String contactUs = "";
-  static String aboutUs = "";
+  static String carousel1 = "assets/images/Slide1.png";
+  static String carousel2 = "assets/images/Slide1.png";
+  static String bookAppoin = "assets/images/BookAppoin.png";
+  static String findDoctor = "assets/images/FindADoc.png";
+  static String labReps = "assets/images/ViewLapReps.png";
+  static String documents = "assets/images/Docs.png";
+  static String appoinList = "assets/images/AppoinList.png";
+  static String uploadDocs = "assets/images/UpldDocs.png";
+  static String callForAppoins = "assets/images/CallForAppoin.png";
+  static String contactUs = "assets/images/ContactUs.png";
+  static String aboutUs = "assets/images/AboutUs.png";
 }
