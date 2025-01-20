7d9d05236453a9a9043b07ce0317d990d140a510
AthiraaAthi <athiraa2612@gmail.com>
2025-01-20 18:59:34 +0530

    added AppointmentListScreen implementation with basic scaffold

---
+++ lib/view/home_screen/screens/appoin_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppointmentListScreen extends StatelessWidget {
+  const AppointmentListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
