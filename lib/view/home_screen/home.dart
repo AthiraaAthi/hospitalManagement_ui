aab9f88e81a2b29abcd145004a741b65453847a2
AthiraaAthi <athiraa2612@gmail.com>
2025-01-20 18:55:49 +0530

    add BookAppointment and FindDoctor screens to HomeScreen

--- lib/view/home_screen/home.dart
+++ lib/view/home_screen/home.dart
@@ -4,6 +4,8 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:hospital_ui/utils/color_constant/color_constant.dart';
 import 'package:hospital_ui/utils/img_constant/image_constant.dart';
+import 'package:hospital_ui/view/home_screen/screens/book_appoinmnt.dart';
+import 'package:hospital_ui/view/home_screen/screens/find_doctor.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -60,7 +62,10 @@ class _HomeScreenState extends State<HomeScreen> {
     ImageConstant.contactUs,
     ImageConstant.aboutUs,
   ];
-  final List<Widget> screens = [];
+  final List<Widget> screens = [
+    BookAppointment(),
+    FindDoctorScreen(),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
