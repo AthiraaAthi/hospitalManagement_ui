3395b5a68934e169b7f7c7e4d145ca12e3aa696f
AthiraaAthi <athiraa2612@gmail.com>
2025-01-20 19:01:08 +0530

    added AppBar to BookAppointment screen scaffold

--- lib/view/home_screen/screens/book_appoinmnt.dart
+++ lib/view/home_screen/screens/book_appoinmnt.dart
@@ -5,6 +5,8 @@ class BookAppointment extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+    );
   }
 }
