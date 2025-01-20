35584ae07cc5c133a4fb4c0376db4b1a84130705
AthiraaAthi <athiraa2612@gmail.com>
2025-01-20 19:01:23 +0530

    set AppBar background color in BookAppointment screen

--- lib/view/home_screen/screens/book_appoinmnt.dart
+++ lib/view/home_screen/screens/book_appoinmnt.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hospital_ui/utils/color_constant/color_constant.dart';
 
 class BookAppointment extends StatelessWidget {
   const BookAppointment({super.key});
@@ -6,7 +7,9 @@ class BookAppointment extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: mainGreen,
+      ),
     );
   }
 }
