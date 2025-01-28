e30733b8d954297bfd49622f8c2264cbbf9778ac
AthiraaAthi <athiraa2612@gmail.com>
2025-01-28 19:24:08 +0530

    added logo image to title row in AboutUsScreen AppBar

--- lib/view/home_screen/screens/about_us.dart
+++ lib/view/home_screen/screens/about_us.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hospital_ui/utils/color_constant/color_constant.dart';
+import 'package:hospital_ui/utils/img_constant/image_constant.dart';
 
 class AboutUsScreen extends StatelessWidget {
   const AboutUsScreen({super.key});
@@ -18,7 +19,18 @@ class AboutUsScreen extends StatelessWidget {
         ),
         centerTitle: true,
         title: Row(
-          children: [],
+          children: [
+            Container(
+              height: 60,
+              width: 60,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(ImageConstant.logo),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
