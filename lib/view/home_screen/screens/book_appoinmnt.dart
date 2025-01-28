b47dee5b35a8bc741f6da4b4cf81a80b31df7fc9
AthiraaAthi <athiraa2612@gmail.com>
2025-01-28 19:14:59 +0530

    replaced Padding with SingleChildScrollView in BookAppointment screen layout

--- lib/view/home_screen/screens/book_appoinmnt.dart
+++ lib/view/home_screen/screens/book_appoinmnt.dart
@@ -29,94 +29,96 @@ class _BookAppointmentState extends State<BookAppointment> {
             style: TextStyle(
                 color: white, fontSize: 19, fontWeight: FontWeight.w600)),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Select a Doctor',
-                hintStyle:
-                    TextStyle(color: mainGreen, fontWeight: FontWeight.w400),
-                suffixIcon: InkWell(
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => DoctorListScreen()));
-                    },
-                    child: Icon(Icons.arrow_circle_down)),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Select a Doctor',
+                  hintStyle:
+                      TextStyle(color: mainGreen, fontWeight: FontWeight.w400),
+                  suffixIcon: InkWell(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => DoctorListScreen()));
+                      },
+                      child: Icon(Icons.arrow_circle_down)),
+                ),
               ),
-            ),
-            space,
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'First Name',
-                hintStyle:
-                    TextStyle(color: mainGreen, fontWeight: FontWeight.w400),
+              space,
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'First Name',
+                  hintStyle:
+                      TextStyle(color: mainGreen, fontWeight: FontWeight.w400),
+                ),
               ),
-            ),
-            space,
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Last Name',
-                hintStyle:
-                    TextStyle(color: mainGreen, fontWeight: FontWeight.w400),
+              space,
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Last Name',
+                  hintStyle:
+                      TextStyle(color: mainGreen, fontWeight: FontWeight.w400),
+                ),
               ),
-            ),
-            space,
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Email',
-                hintStyle:
-                    TextStyle(color: mainGreen, fontWeight: FontWeight.w400),
+              space,
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Email',
+                  hintStyle:
+                      TextStyle(color: mainGreen, fontWeight: FontWeight.w400),
+                ),
               ),
-            ),
-            space,
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Phone',
-                hintStyle:
-                    TextStyle(color: mainGreen, fontWeight: FontWeight.w400),
+              space,
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Phone',
+                  hintStyle:
+                      TextStyle(color: mainGreen, fontWeight: FontWeight.w400),
+                ),
               ),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Text("Available Dates",
-                style: TextStyle(
-                    color: mainGreen,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w400)),
-            space,
-            Text("Available Times / Serial No",
-                style: TextStyle(
-                    color: mainGreen,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w400)),
-            space,
-            Text("Problems",
-                style: TextStyle(
-                    color: mainGreen,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w400)),
-            space,
-            TextField(
-              maxLines: 5,
-              textAlign: TextAlign.center,
-              decoration: InputDecoration(
-                labelText: "Enter Your Problem",
-                border: OutlineInputBorder(),
+              SizedBox(
+                height: 30,
               ),
-            )
-          ],
+              Text("Available Dates",
+                  style: TextStyle(
+                      color: mainGreen,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400)),
+              space,
+              Text("Available Times / Serial No",
+                  style: TextStyle(
+                      color: mainGreen,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400)),
+              space,
+              Text("Problems",
+                  style: TextStyle(
+                      color: mainGreen,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400)),
+              space,
+              TextField(
+                maxLines: 5,
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                  labelText: "Enter Your Problem",
+                  border: OutlineInputBorder(),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
