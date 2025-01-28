2f50af9b8528048512534de4ec30ad1cadcd5c23
AthiraaAthi <athiraa2612@gmail.com>
2025-01-28 19:24:29 +0530

    added empty Column widget to AboutUsScreen layout

--- lib/view/home_screen/screens/about_us.dart
+++ lib/view/home_screen/screens/about_us.dart
@@ -30,6 +30,9 @@ class AboutUsScreen extends StatelessWidget {
                 ),
               ),
             ),
+            Column(
+              children: [],
+            ),
           ],
         ),
       ),
