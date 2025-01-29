24bab85bee65ca26e78c7deb59fe356c63eac0b0
AthiraaAthi <athiraa2612@gmail.com>
2025-01-29 18:59:12 +0530

    add additional context text to AboutUsScreen

--- lib/view/home_screen/screens/about_us.dart
+++ lib/view/home_screen/screens/about_us.dart
@@ -100,6 +100,8 @@ class _AboutUsScreenState extends State<AboutUsScreen> {
               "Where does it come from?",
               style: TextStyle(fontSize: 15),
             ),
+            Text(
+                "Contrary, to popular belief. Loreum Ipsum is not simply random text. It has roots on a belief of ..........................")
           ],
         ),
       ),
