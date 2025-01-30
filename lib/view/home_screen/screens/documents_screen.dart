ca0aacf68601bea7b4c5644abd41b09dac229ad1
AthiraaAthi <athiraa2612@gmail.com>
2025-01-30 18:35:59 +0530

    added background color to AppBar in DocumentsScreen for improved aesthetics

--- lib/view/home_screen/screens/documents_screen.dart
+++ lib/view/home_screen/screens/documents_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hospital_ui/utils/color_constant/color_constant.dart';
 
 class DocumentsScreen extends StatelessWidget {
   const DocumentsScreen({super.key});
@@ -6,7 +7,9 @@ class DocumentsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: mainGreen,
+      ),
     );
   }
 }
