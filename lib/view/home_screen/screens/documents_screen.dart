d22ba8042eb5c995c701bfa8447201c44c21e2c8
AthiraaAthi <athiraa2612@gmail.com>
2025-01-30 18:35:00 +0530

    added AppBar to Scaffold in DocumentsScreen for improved navigation

--- lib/view/home_screen/screens/documents_screen.dart
+++ lib/view/home_screen/screens/documents_screen.dart
@@ -5,6 +5,8 @@ class DocumentsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+    );
   }
 }
