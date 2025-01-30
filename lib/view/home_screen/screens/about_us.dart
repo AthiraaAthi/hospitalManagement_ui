97d76a7fc42907864d63f9523dd9084a43caa5e9
AthiraaAthi <athiraa2612@gmail.com>
2025-01-30 18:31:39 +0530

    added alarm icon to Row widget in AboutUsScreen for visual enhancement

--- lib/view/home_screen/screens/about_us.dart
+++ lib/view/home_screen/screens/about_us.dart
@@ -147,7 +147,12 @@ class _AboutUsScreenState extends State<AboutUsScreen> {
                   "Contrary, to popular belief. Loreum Ipsum is not simply random text. It has roots on a belief of .........................."),
               space,
               Row(
-                children: [],
+                children: [
+                  Icon(
+                    Icons.alarm_sharp,
+                    size: 20,
+                  ),
+                ],
               )
             ],
           ),
