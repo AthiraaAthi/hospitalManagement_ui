2af3f820e93e35dcca476bde94d31584a3f54b86
AthiraaAthi <athiraa2612@gmail.com>
2025-01-31 20:31:58 +0530

    added vertical spacing with SizedBox in DocumentsScreen for improved layout

--- lib/view/home_screen/screens/documents_screen.dart
+++ lib/view/home_screen/screens/documents_screen.dart
@@ -77,6 +77,9 @@ class DocumentsScreen extends StatelessWidget {
                         fontSize: 15,
                         fontWeight: FontWeight.w500),
                   ),
+                  SizedBox(
+                    height: 20,
+                  ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
