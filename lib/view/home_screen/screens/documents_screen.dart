086e6ac84fb6877a0388d14b93b1cf5d2e0a781a
AthiraaAthi <athiraa2612@gmail.com>
2025-02-01 19:32:35 +0530

    refactored DocumentsScreen to use ListView for improved performance and layout

--- lib/view/home_screen/screens/documents_screen.dart
+++ lib/view/home_screen/screens/documents_screen.dart
@@ -27,115 +27,117 @@ class DocumentsScreen extends StatelessWidget {
         ),
         child: Column(
           children: [
-            Container(
-              padding: EdgeInsets.all(10),
-              height: 250,
-              width: double.infinity,
-              decoration: BoxDecoration(border: Border.all(color: black)),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Container(
-                    height: 30,
-                    width: 300,
-                    decoration: BoxDecoration(
-                      color: mainGreen,
+            ListView.builder(
+              itemBuilder: (context, index) => Container(
+                padding: EdgeInsets.all(10),
+                height: 250,
+                width: double.infinity,
+                decoration: BoxDecoration(border: Border.all(color: black)),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 10,
                     ),
-                    child: Center(
-                      child: Text("Patient ID :RCVKAJNAHNAJNA",
-                          style: TextStyle(
-                              color: white,
-                              fontSize: 13,
-                              fontWeight: FontWeight.w500)),
+                    Container(
+                      height: 30,
+                      width: 300,
+                      decoration: BoxDecoration(
+                        color: mainGreen,
+                      ),
+                      child: Center(
+                        child: Text("Patient ID :RCVKAJNAHNAJNA",
+                            style: TextStyle(
+                                color: white,
+                                fontSize: 13,
+                                fontWeight: FontWeight.w500)),
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text("Dr.Ryle Kincaid",
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text("Dr.Ryle Kincaid",
+                        style: TextStyle(
+                            color: black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500)),
+                    Text(
+                      "Assistant Professor",
                       style: TextStyle(
-                          color: black,
-                          fontSize: 20,
-                          fontWeight: FontWeight.w500)),
-                  Text(
-                    "Assistant Professor",
-                    style: TextStyle(
-                        color: Colors.grey,
-                        fontSize: 15,
-                        fontWeight: FontWeight.w500),
-                  ),
-                  Text(
-                    "Date : 31-01-2025",
-                    style: TextStyle(
-                        color: Colors.grey,
-                        fontSize: 15,
-                        fontWeight: FontWeight.w500),
-                  ),
-                  Text(
-                    "Updated by : Patient",
-                    style: TextStyle(
-                        color: Colors.grey,
-                        fontSize: 15,
-                        fontWeight: FontWeight.w500),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        height: 40,
-                        width: 100,
-                        decoration: BoxDecoration(
-                          color: black,
-                          borderRadius: BorderRadius.circular(5),
+                          color: Colors.grey,
+                          fontSize: 15,
+                          fontWeight: FontWeight.w500),
+                    ),
+                    Text(
+                      "Date : 31-01-2025",
+                      style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 15,
+                          fontWeight: FontWeight.w500),
+                    ),
+                    Text(
+                      "Updated by : Patient",
+                      style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 15,
+                          fontWeight: FontWeight.w500),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          height: 40,
+                          width: 100,
+                          decoration: BoxDecoration(
+                            color: black,
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          child: Center(
+                            child: Text("view Profile",
+                                style: TextStyle(
+                                    color: white,
+                                    fontSize: 13,
+                                    fontWeight: FontWeight.w500)),
+                          ),
                         ),
-                        child: Center(
-                          child: Text("view Profile",
-                              style: TextStyle(
-                                  color: white,
-                                  fontSize: 13,
-                                  fontWeight: FontWeight.w500)),
+                        Container(
+                          height: 40,
+                          width: 100,
+                          decoration: BoxDecoration(
+                            color: mainGreen,
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          child: Center(
+                            child: Text("Edit",
+                                style: TextStyle(
+                                    color: white,
+                                    fontSize: 13,
+                                    fontWeight: FontWeight.w500)),
+                          ),
                         ),
-                      ),
-                      Container(
-                        height: 40,
-                        width: 100,
-                        decoration: BoxDecoration(
-                          color: mainGreen,
-                          borderRadius: BorderRadius.circular(5),
-                        ),
-                        child: Center(
-                          child: Text("Edit",
-                              style: TextStyle(
-                                  color: white,
-                                  fontSize: 13,
-                                  fontWeight: FontWeight.w500)),
-                        ),
-                      ),
-                      Container(
-                        height: 40,
-                        width: 100,
-                        decoration: BoxDecoration(
-                          color: Colors.red,
-                          borderRadius: BorderRadius.circular(5),
-                        ),
-                        child: Center(
-                          child: Text("Delete",
-                              style: TextStyle(
-                                  color: white,
-                                  fontSize: 13,
-                                  fontWeight: FontWeight.w500)),
-                        ),
-                      )
-                    ],
-                  )
-                ],
+                        Container(
+                          height: 40,
+                          width: 100,
+                          decoration: BoxDecoration(
+                            color: Colors.red,
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          child: Center(
+                            child: Text("Delete",
+                                style: TextStyle(
+                                    color: white,
+                                    fontSize: 13,
+                                    fontWeight: FontWeight.w500)),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
               ),
             )
           ],
