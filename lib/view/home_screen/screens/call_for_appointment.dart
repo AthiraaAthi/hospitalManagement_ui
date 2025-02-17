4b73e08e1fa2202e6a7466b97c5763ac5d73a702
AthiraaAthi <athiraa2612@gmail.com>
2025-02-17 18:43:59 +0530

    add logo to CallForAppointment screen AppBar

--- lib/view/home_screen/screens/call_for_appointment.dart
+++ lib/view/home_screen/screens/call_for_appointment.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hospital_ui/utils/color_constant/color_constant.dart';
+import 'package:hospital_ui/utils/img_constant/image_constant.dart';
 
 class CallForAppointment extends StatelessWidget {
   const CallForAppointment({super.key});
@@ -11,6 +12,20 @@ class CallForAppointment extends StatelessWidget {
         backgroundColor: white,
         toolbarHeight: 100,
         centerTitle: true,
+        title: Row(
+          children: [
+            Container(
+              height: 60,
+              width: 60,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(ImageConstant.logo),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
