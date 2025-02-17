0fd069b06b2e43fa7ee555d2119f4ce41810d942
AthiraaAthi <athiraa2612@gmail.com>
2025-02-17 18:43:10 +0530

    add AppBar to CallForAppointment screen

--- lib/view/home_screen/screens/call_for_appointment.dart
+++ lib/view/home_screen/screens/call_for_appointment.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:hospital_ui/utils/color_constant/color_constant.dart';
 
 class CallForAppointment extends StatelessWidget {
   const CallForAppointment({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: white,
+        toolbarHeight: 100,
+        centerTitle: true,
+      ),
+    );
   }
 }
